6b96abb61e5e0e7b3e65f517671c0a86bb9567a5
Apurva Mishra <a0mish06@louisville.edu>
2025-12-14 23:05:29 -0500

    learning vars

--- Program.cs
+++ Program.cs
@@ -1,3 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("The date and time is: " + DateTime.Now);
+﻿int firstName;
+firstName = "Bob";
