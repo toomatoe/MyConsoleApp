da19c42f978a749798823898b1b1fc5fc1097cda
Apurva Mishra <a0mish06@louisville.edu>
2025-12-18 23:29:06 -0500

    idk

--- ArrayProject.cs
+++ ArrayProject.cs
@@ -1,4 +1,9 @@
-string[] fraudulentOrderIDs = new string[3];
+
+public class Program
+{
+    public static void Main(string[] args)
+{
+    string[] fraudulentOrderIDs = new string[3];
 fraudulentOrderIDs[0] = "A123";
 fraudulentOrderIDs[1] = "B456";
 fraudulentOrderIDs[2] = "C789";
@@ -7,6 +12,7 @@ fraudulentOrderIDs[2] = "C789";
 Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
 Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
 Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
-
+}
+}
 
 
