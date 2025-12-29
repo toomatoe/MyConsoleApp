74147f92384b617e7573c05d4e481fff3821d3b5
Apurva Mishra <a0mish06@louisville.edu>
2025-12-28 22:44:29 -0500

    learned foreach

--- ArrayProject.cs
+++ ArrayProject.cs
@@ -2,21 +2,21 @@
 public class Array
 {
     public static void Main(string[] args)
-{
-    string[] fraudulentOrderIDs = new string[3];
-fraudulentOrderIDs[0] = "A123";
-fraudulentOrderIDs[1] = "B456";
-fraudulentOrderIDs[2] = "C789";
-// fraudulentOrderIDs[3] = "D000";
+    {
+    int sum = 0;
+    int[] inventory = { 200, 450, 700, 175, 250 };
+    int bin = 0;
+    foreach (int item in inventory){
+        sum += item;
+        bin++;
+        Console.WriteLine($"Bin {bin} = {item} items (Running total: {sum})");
+    }
+    Console.WriteLine($"We have {sum} items in inventory.");
+    
 
-Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
-Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
-Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
-
-
-
-        }
 }
+}
+
         
 
   
